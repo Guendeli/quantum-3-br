a05de9ba2aa0ffad0887821253b7c04dc075b08b
Omar <omar.guendeli@ubisoft.com>
2025-01-08 13:29:23 +0400

    added debug input, player view instantiation on player added

--- Assets/QuantumUser/Simulation/Systems/CharacterMovableSystem.cs
+++ Assets/QuantumUser/Simulation/Systems/CharacterMovableSystem.cs
@@ -3,18 +3,44 @@ namespace Quantum
     using Photon.Deterministic;
     using UnityEngine.Scripting;
 
+    /// <summary>
+    /// System that moves all entities with CharacterController components,
+    /// Also instantiate default player when a player is added to the room.
+    /// </summary>
     [Preserve]
-    public unsafe class CharacterMovableSystem : SystemMainThreadFilter<CharacterMovableSystem.Filter>
+    public unsafe class CharacterMovableSystem : SystemMainThreadFilter<CharacterMovableSystem.Filter>, ISignalOnPlayerAdded
     {
         public override void Update(Frame f, ref Filter filter)
         {
-            filter.CharacterController3D->Move(f, filter.Entity, default);
+            Input* playerInput = f.GetPlayerInput(filter.PlayerLink->Player);
+            FPVector3 direction = playerInput->Direction.XOY;
+            
+            // cheat check
+            if (direction.Magnitude > 1)
+            {
+                direction = direction.Normalized;
+            }
+            
+            filter.CharacterController3D->Move(f, filter.Entity, direction);
         }
 
         public struct Filter
         {
             public EntityRef Entity;
             public CharacterController3D* CharacterController3D;
+            public PlayerLink* PlayerLink;
+        }
+
+        public void OnPlayerAdded(Frame f, PlayerRef player, bool firstTime)
+        {
+            RuntimePlayer playerData = f.GetPlayerData(player);
+            EntityRef playerEntity = f.Create(playerData.PlayerAvatar);
+            PlayerLink playerLink = new PlayerLink()
+            {
+                Player = player
+            };
+            
+            f.Add(playerEntity, playerLink);
         }
     }
 }
